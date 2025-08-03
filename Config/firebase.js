d92750a8cd5e09c980fe0752f7c266fabcc8ed8a
sky33-git <sunilsks2017@gmail.com>
2025-08-03 12:41:58 +0530

    FEAT: Firebase fix

--- Config/firebase.js
+++ Config/firebase.js
@@ -1,8 +1,16 @@
-import admin from "firebase-admin"
-import { serviceAccount } from "../serviceAccount.js";
+import admin from "firebase-admin";
+import dotenv from "dotenv";
+import path from "path";
+import { readFileSync } from "fs";
+
+dotenv.config();
+
+const serviceAccountPath = path.resolve(process.env.SERVICE_ACCOUNT_PATH);
+const serviceAccount = JSON.parse(readFileSync(serviceAccountPath, "utf8"));
+
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
+  credential: admin.credential.cert(serviceAccount),
 });
 
-export default admin
+export default admin;
