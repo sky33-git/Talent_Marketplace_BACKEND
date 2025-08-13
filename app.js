2e252e7f7b0551f88d6d159387cd51555cf98c00
sky33-git <sunilsks2017@gmail.com>
2025-08-14 00:51:31 +0530

    Update app.js

--- app.js
+++ app.js
@@ -27,7 +27,8 @@ const app = express()
 app.use(cookieParser())
 app.use(express.json())
 app.use(cors({
-    origin: `${BASE_API}`,
+    origin: [`${BASE_API}`,
+    "http://localhost:5173"],
     credentials: true,
     methods:["GET", "POST", "PUT", "DELETE"]
 }))
